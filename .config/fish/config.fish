c9d10476654448125b9e3648b138596c799356e0
fox0430 <ipoduser0430@gmail.com>
2017-08-16 23:41:24 +0000

    backup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,33 @@ alias DL "cd & cd ~/Downloads"
 alias x "startx"
 alias py "python"
 alias rm "rm -i"
+
+function cd
+    # Avoid set completions.
+    set -l previous $PWD
+
+    if test "$argv" = "-"
+        if test "$__fish_cd_direction" = "next"
+            nextd
+        else
+            prevd
+        end
+        return $status
+    end
+    builtin cd $argv
+    set -l cd_status $status
+    # Log history
+    if test $cd_status -eq 0 -a "$PWD" != "$previous"
+        set -q dirprev[$MAX_DIR_HIST]
+        and set -e dirprev[1]
+        set -g dirprev $dirprev $previous
+        set -e dirnext
+        set -g __fish_cd_direction prev
+    end
+
+    if test $cd_status -ne 0
+        return 1
+    end
+    ls
+    return $status
+end
