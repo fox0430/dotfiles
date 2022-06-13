bacf993b74455f4e9e3045f55f7e44036a4fbab2
fox0430 <shuu.n@protonmail.com>
2022-06-14 05:00:21 +0900

    xmonad: Change the default terminal to alacritty

---
+++ .xmonad/alacritty_float.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#=#=#=
+# alacritty with `alacritty_float` name.
+#
+# For xmonad float window handling.
+#=#=
+
+alacritty --title alacritty_float
--- .xmonad/urxvt_float.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#=#=#=
-# urxvt with `urxvt_float` name.
-#
-# For xmonad float window handling.
-#=#=
-
-urxvt -g 90x25 -name "urxvt_float" "$@"
